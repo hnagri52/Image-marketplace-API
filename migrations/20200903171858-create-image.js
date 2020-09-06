c7564f10c57d6b93660aefb67b05a26cff0cf277
Hussein <hznagri@uwaterloo.ca>
2020-09-06 17:05:23 -0400

    model and migration

---
+++ migrations/20200903171858-create-image.js
@@ -0,0 +1,57 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable("Images", {
+      id: {
+        primaryKey: true,
+        allowNull: false,
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+      },
+      userId: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      isPrivate: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false,
+      },
+      discountPercentage: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable("Images");
+  },
+};
---
+++ models/image_model.js
@@ -0,0 +1,28 @@
+"use strict";
+
+/* DB Image model */
+module.exports = (sequelize, DataTypes) => {
+  const Image = sequelize.define(
+    "Image",
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      userId: DataTypes.UUID,
+      name: DataTypes.STRING,
+      description: DataTypes.STRING,
+      url: DataTypes.STRING,
+      isPrivate: DataTypes.BOOLEAN,
+      quantity: DataTypes.INTEGER,
+      price: DataTypes.DOUBLE,
+      discountPercentage: DataTypes.FLOAT,
+    },
+    {}
+  );
+  Image.associate = function (models) {
+    // associations can be defined here
+  };
+  return Image;
+};
