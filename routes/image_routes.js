deb3d67a2df653846f2172f789a84e51454aaaf1
Hussein <hznagri@uwaterloo.ca>
2020-09-06 17:41:36 -0400

    added routes and packages for auth

--- routes/image_routes.js
+++ routes/image_routes.js
@@ -4,6 +4,12 @@ var router = express.Router();
 const multerUpload = require("../middleware/multer");
 const images = require("../controllers/image_controller");
 
+/* GET Retrieve all images. */
+router.get("/", images.getAllImages);
+
+/* GET Retrieve all user images. */
+router.get("/:userId", images.getUserImages);
+
 /* POST Upload Image. */
 router.post("/", multerUpload.array("image"), images.postImages);
 
