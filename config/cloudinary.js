3b8bfb90dea4a99ab20b32766030c4eb25db680d
Hussein <hznagri@uwaterloo.ca>
2020-09-07 17:21:23 -0400

    cludinary setup

---
+++ config/cloudinary.js
@@ -0,0 +1,30 @@
+const dotenv = require("dotenv");
+const cloudinary = require("cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API,
+  api_secret: process.env.CLOUDINARY_SECRET,
+});
+
+const uploadCloudinary = (file, folder) => {
+  return new Promise((resolve) => {
+    cloudinary.uploader.upload(
+      file,
+      (result) => {
+        resolve({
+          url: result.url,
+          id: result.public_id,
+        });
+      },
+      {
+        resource_type: "auto",
+        folder: folder,
+      }
+    );
+  });
+};
+
+module.exports = {
+  uploadCloudinary,
+};
