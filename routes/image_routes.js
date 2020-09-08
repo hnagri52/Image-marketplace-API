e30c8a16a0606c3609b5c5b78f27998b846eec9e
Hussein <hznagri@uwaterloo.ca>
2020-09-07 21:18:31 -0400

    update routes

--- routes/image_routes.js
+++ routes/image_routes.js
@@ -3,12 +3,20 @@ var express = require("express");
 var router = express.Router();
 const multerUpload = require("../middleware/multer");
 const images = require("../controllers/image_controller");
+var passport = require("passport");
+require("../middleware/passport");
 
 /* GET Retrieve all images. */
 router.get("/", images.getAllImages);
 
 /* GET Retrieve all user images. */
-router.get("/:userId", images.getUserImages);
+router.get("/private", passport.authenticate("jwt", { session: false }), images.getUserImages);
+
+/* GET Image by ID. */
+router.get("/:imageId", passport.authenticate("jwt", { session: false }), images.getImageDetails);
+
+/* PATCH update image by id. */
+router.patch("/:imageId", passport.authenticate("jwt", { session: false }), images.patchImage);
 
 /* POST Upload Image. */
 router.post("/", multerUpload.array("image"), images.postImages);
